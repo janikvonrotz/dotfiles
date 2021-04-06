ef16d715fdb39e2858609b1285de2b0e87ef8018
Janik von Rotz <login@janikvonrotz.ch>
2021-04-06 20:23:04 +0200

    zsh

--- oh-my-zsh-custom/env.zsh
+++ oh-my-zsh-custom/env.zsh
@@ -6,14 +6,6 @@ export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 
-# fzf
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-
-# zmux
-if [ -z "$TMUX" ]; then
-   # tmux attach -t default || tmux new -s default
-fi
-
 # pip user
 export PATH="$HOME/.local/bin:$PATH"
 

b9ddc139af1561f347335e859ca986d5ea0ae835
Janik von Rotz <login@janikvonrotz.ch>
2024-02-15 16:04:46 +0100

    remove aliases

--- oh-my-zsh-custom/alias.zsh
+++ oh-my-zsh-custom/alias.zsh
@@ -8,14 +8,5 @@ alias dps="docker ps --format 'table {{.Names}}\t{{(.Label \"description\")}}'"
 alias dkl="docker logs -f --tail 10"
 alias dkp="docker ps"
 
-# Python
-alias python="python3"
-
 # fzf
 alias fzv='vim $(fzf)'
-
-# Code
-alias code="flatpak run com.vscodium.codium"
-
-# Commands
-#function pwgen() {< /dev/urandom tr -dc A-Za-z0-9 | head -c"$1"; echo}

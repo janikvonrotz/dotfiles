140a369d03c573379df23d5cd78a3be451720f94
Janik von Rotz <login@janikvonrotz.ch>
2024-07-11 10:09:09 +0200

    feat: remove omz plugins

--- oh-my-zsh/custom/config.zsh
+++ oh-my-zsh/custom/config.zsh
@@ -1,7 +1,1 @@
-# Git
-# git config --global user.email "login@janikvonrotz.ch"
-# git config --global user.name "Janik von Rotz"
-# git config --global alias.co checkout
-# git config --global alias.br branch
-# git config --global alias.ci commit
-# git config --global alias.st status
+export PATH=~/.local/bin:$PATH

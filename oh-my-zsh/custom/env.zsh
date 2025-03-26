1145d199b5aadabc3de1996f37d77ef2c114b524
Janik von Rotz <login@janikvonrotz.ch>
2025-03-26 17:55:22 +0100

    feat: init syncthing

--- oh-my-zsh/custom/env.zsh
+++ oh-my-zsh/custom/env.zsh
@@ -1,4 +1,10 @@
 export EDITOR=hx
+
 export DISABLE_UPDATE_PROMPT=true
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+if [ -d "$HOME/Sync" ]; then
+  export HISTFILE="$HOME/Sync/.zsh_history"
+fi

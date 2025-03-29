aa37b8e245d393d3f199b2756abef7d9f9ce2587
Janik von Rotz <login@janikvonrotz.ch>
2025-03-29 14:28:39 +0100

    feat(zsh): sync history with nextcloud

--- oh-my-zsh/custom/env.zsh
+++ oh-my-zsh/custom/env.zsh
@@ -5,6 +5,6 @@ export DISABLE_UPDATE_PROMPT=true
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-if [ -d "$HOME/Sync" ]; then
-  export HISTFILE="$HOME/Sync/.zsh_history"
+if [ -d "$HOME/janikv.cloud/home/$USERNAME" ]; then
+  export HISTFILE="$HOME/janikv.cloud/home/$USERNAME/.zsh_history"
 fi

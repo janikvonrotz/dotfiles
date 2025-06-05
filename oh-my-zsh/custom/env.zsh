0553a2f095244af56cc931e7783a9eb4e7e657f7
Janik von Rotz <login@janikvonrotz.ch>
2025-06-05 10:20:32 +0200

    feat: flatpak env vars

--- oh-my-zsh/custom/env.zsh
+++ oh-my-zsh/custom/env.zsh
@@ -8,3 +8,5 @@ export NVM_DIR="$HOME/.nvm"
 if [ -d "$HOME/janikv.cloud/home/$USERNAME" ]; then
   export HISTFILE="$HOME/janikv.cloud/home/$USERNAME/.zsh_history"
 fi
+
+XDG_DATA_DIRS="$XDG_DATA_DIRS:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share"

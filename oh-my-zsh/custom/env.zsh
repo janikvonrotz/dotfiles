96527dc468a77ce52125d6450127d164697d62dd
Janik von Rotz <login@janikvonrotz.ch>
2025-06-22 11:49:10 +0200

    feat(env): configure default editor, disable update prompts, and set up ssh agent

--- oh-my-zsh/custom/env.zsh
+++ oh-my-zsh/custom/env.zsh
@@ -1,12 +1,16 @@
+# Default editor
 export EDITOR=hx
 
+# Disable oh-my-zsh update prompt
 export DISABLE_UPDATE_PROMPT=true
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
+# Link zsh history to nexctloud folder
 if [ -d "$HOME/janikv.cloud/home/$USERNAME" ]; then
   export HISTFILE="$HOME/janikv.cloud/home/$USERNAME/.zsh_history"
 fi
 
+# Flatpak share
 XDG_DATA_DIRS="$XDG_DATA_DIRS:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share"
+
+# SSH agent
+export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
